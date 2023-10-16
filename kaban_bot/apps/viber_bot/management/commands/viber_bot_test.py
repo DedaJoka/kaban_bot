b63709db858c2e8b86cc84c2d6311257c02a22b3
DedaJoka <horban.vadym@gmail.com>
2023-10-16 08:28:50 +0300

    test cron?

--- kaban_bot/apps/viber_bot/management/commands/viber_bot_test.py
+++ kaban_bot/apps/viber_bot/management/commands/viber_bot_test.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 
 from rabbitmq.management.commands.package_creator import CustomCreate
 from ...models import ViberUser, Service
+from rabbitmq.models import RabbitPackage
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
@@ -11,17 +12,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print(f"Запустили команду test")
-
-        service = Service.objects.filter()
-        for s in service:
-            class ServiceSerializer(serializers.ModelSerializer):
-                class Meta:
-                    model = Service
-                    fields = ['status_code', 'id', 'name', 'parent']
-
-            service_serializer = ServiceSerializer(s)
-            service_json_data = service_serializer.data
-            json_data = json.dumps(service_json_data, ensure_ascii=False).encode('utf-8')
-            decoded_json_data = json_data.decode('utf-8')
-            new_package = CustomCreate.create_package(s.id, 'INSERT', 'application/json', 'kvb::service',
-                                                      decoded_json_data)
+        new_package = RabbitPackage(direction=1,
+                                    priority=1,
+                                    status_code=3,
+                                    version=456,
+                                    identifier='test',
+                                    operation='test',
+                                    contentType='test',
+                                    type='test',
+                                    body='test')
+        new_package.save()
