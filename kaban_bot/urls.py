add4271b7d1230d7a59a61d845f581b67e68a833
DedaJoka <horban.vadym@gmail.com>
2023-10-16 12:51:18 +0300

    update

--- kaban_bot/urls.py
+++ kaban_bot/urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
     path('', include('viber_bot.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
